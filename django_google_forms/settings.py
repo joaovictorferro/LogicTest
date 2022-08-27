b9cc7b32ff7b279b3ca10b64e92eb649b139bfba
João Victor Ribeiro Ferro <39158742+joaovictorferro@users.noreply.github.com>
2022-08-27 16:19:52 -0300

    Update settings.py

--- django_google_forms/settings.py
+++ django_google_forms/settings.py
@@ -32,18 +32,13 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
-    'djangocricket.Cricket',
-    'djangocricket.cms'
+    'g_forms'
 ]
 
 MIDDLEWARE = [
